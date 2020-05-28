db17353a9774b920289ec1527e51f1ba86fd5774
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-28 21:12:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,12 @@
 class Dog 
+  def bark
+      puts "Woof!"
+  end
 end
 
 fido = Dog.new
-#snoopy = Dog.new 
-#lassie = Dog.new 
+fido.bark
 
-
-def bark()
-  fido.bark
-  puts "Woof!"
-end
 
 def sit ()
   fido.sit
